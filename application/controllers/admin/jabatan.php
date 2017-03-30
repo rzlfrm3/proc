204d342a5a85f964243f5edc154e567f7d032ede
rzlfrm3 <rzlfrm3@gmail.com>
2017-03-30 22:51:34 +0700

    Update jabatan.php

--- application/controllers/admin/jabatan.php
+++ application/controllers/admin/jabatan.php
@@ -39,7 +39,7 @@ class Jabatan extends CI_Controller {
 		"id_jabatan" => $mhs[0]['id_jabatan'],
 		"nama_jabatan" => $mhs[0]['nama_jabatan'],
 		);
-		$data['title'] = 'Notifikasi seperti difacebook CodeIgniter'; //judul title
+		$data['title'] = 'Notifikasi seperti difacebook CodeIgniter'; //judul e
         $data['jlhnotif'] =$this->model_notifikasi->notif_count();  //menghitung jumlah post
         $data['notifikasi'] =$this->model_notifikasi->getnotifikasi(); //menampilkan isi postingan
 		$data['data'] = $this->model_jabatan->GetUser();
