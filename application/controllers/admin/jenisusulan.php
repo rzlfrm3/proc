e100e723128d0919e2fe56eaf66be33af36725f6
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-03 14:04:08 +0700

    Update jenisusulan.php

--- application/controllers/admin/jenisusulan.php
+++ application/controllers/admin/jenisusulan.php
@@ -7,7 +7,7 @@ class Jenisusulan extends CI_Controller {
 	{
 		$data['title'] = 'Notifikasi seperti difacebook CodeIgniter'; //judul title
         $data['jlhnotif'] =$this->model_notifikasi->notif_count();  //menghitung jumlah post
-        $data['notifikasi'] =$this->model_notifikasi->getnotifikasi(); //menampilkan isi postingan
+        $data['notifikasi'] =$this->model_notifikasi->getnotifikasi(); 
 		$data ['data']= $this->model_jenisusulan->GetUser();
 		$this->load->view('admin/tabel_jenisusulan', $data);
 	}
