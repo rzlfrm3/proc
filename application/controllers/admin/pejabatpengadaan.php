92d267ea7c60fa3f7fbda9d86e9585e59f47e1f4
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-03 22:47:42 +0700

    Update pejabatpengadaan.php

--- application/controllers/admin/pejabatpengadaan.php
+++ application/controllers/admin/pejabatpengadaan.php
@@ -5,7 +5,7 @@ class Pejabatpengadaan extends CI_Controller {
 
 	public function index()
 	{
-		$data['title'] = 'Notifikasi seperti difacebook CodeIgniter'; //judul title
+		$data['title'] = 'Notifikasi seperti difacebook CodeIgniter'; 
         $data['jlhnotif'] =$this->model_notifikasi->notif_count();  //menghitung jumlah post
         $data['notifikasi'] =$this->model_notifikasi->getnotifikasi(); //menampilkan isi postingan
 		$data['data'] = $this->model_pejabatpengadaan->GetUser();
