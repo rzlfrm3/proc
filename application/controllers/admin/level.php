c61be560b6259b962fbe036c336370a22a5d93c6
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-04 22:04:42 +0700

    Update level.php

--- application/controllers/admin/level.php
+++ application/controllers/admin/level.php
@@ -14,7 +14,7 @@ class Level extends CI_Controller {
 	public function add_data(){
 		$data['title'] = 'Notifikasi seperti difacebook CodeIgniter'; //judul title
         $data['jlhnotif'] =$this->model_notifikasi->notif_count();  //menghitung jumlah post
-        $data['notifikasi'] =$this->model_notifikasi->getnotifikasi(); //menampilkan isi postingan
+        $data['notifikasi'] =$this->model_notifikasi->getnotifikasi(); //menampilkan isi posting
 		$data ['data']= $this->model_level->GetUser();
 		$this->load->view('admin/form_tambahlevel',$data);
 	}
