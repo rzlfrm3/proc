46ed0fa01f1721babed70bce7b294930a771e850
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-05 20:52:28 +0700

    Update form_editalternatif.php

--- application/views/admin/form_editalternatif.php
+++ application/views/admin/form_editalternatif.php
@@ -35,7 +35,7 @@
             <div class="profile clearfix">
               <div class="profile_pic">
                 <img src="<?php echo base_url()."assets/"; ?>production/images/img.jpg" alt="..." class="img-circle profile_img">
-              </div> ----
+              </div> ---
               <div class="profile_info">
                 <span>Welcome,</span>
                 <h2>John Doe</h2>
