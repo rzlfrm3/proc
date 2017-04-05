7f8ce538980fd556b4cab8af28919a1f81f8f64d
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-05 20:37:17 +0700

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /*
 | ----------------------------------------------------------------------
 | AUTO-LOADER
-| -------------------------------------------------------------------
+| ----------------------------------------------------------------------
 | This file specifies which systems should be loaded by default.
 |
 | In order to keep the framework as light-weight as possible only the
