bbbdb38835cb729c9d50ca94408a2e90aa3686e4
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-06 22:49:00 +0700

    Update alternatif.php

--- application/controllers/admin/alternatif.php
+++ application/controllers/admin/alternatif.php
@@ -5,7 +5,7 @@ class Alternatif extends CI_Controller {
 	
 	public function index() {
 		
-       $data['title'] = 'Notifikasi seperti difacebook CodeIgniter';  // 
+       $data['title'] = 'Notifikasi ';  // 
        $data['jlhnotif'] =$this->model_notifikasi->notif_count();   
        $data['notifikasi'] =$this->model_notifikasi->getnotifikasi(); 
 	   $data['data'] = $this->model_alternatif->GetUser();
