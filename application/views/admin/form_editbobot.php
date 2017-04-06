3b371fcba64bd2ba9026bcdc03111929db850ec6
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-06 23:02:05 +0700

    Update form_editbobot.php

--- application/views/admin/form_editbobot.php
+++ application/views/admin/form_editbobot.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Gentelella Alela! | </title> hgjgjhgjhgjgjhg gjhgjhg gjhgjh jgjg j
+    <title>Gentelella Alela! | </title> hgjgjhgjhgjgjhg gjhgjhg gjhgjh jgjg
 
     <!-- Bootstrap -->
     <link href="<?php echo base_url()."assets/"; ?>vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
