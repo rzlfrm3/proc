6a244b878b36240f561fddb28a27d647f1134812
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-09 22:10:42 +0700

    Update model_jenisusulan.php

--- application/models/model_jenisusulan.php
+++ application/models/model_jenisusulan.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_jenisusulan extends CI_Model {
 	public function GetUser($where=""){
-		$data = $this->db->query('select * from tb_jenisusulan '.$where);
+		$data = $this->db->query('select * from tb_jenisusulan '.$where); //
 		return $data->result_array();
 	}
 	public function insertdata($tabelName,$data){
