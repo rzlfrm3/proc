c19bfbddc395b7b73ffbb3d7e99f11190b1537fc
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-09 22:09:18 +0700

    Update model_graph.php

--- application/models/model_graph.php
+++ application/models/model_graph.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Model_graph extends CI_Model {
 public function __construct()
 {
-$this->load->database();
+$this->load->database(); //
 }
  
 public function graph()
