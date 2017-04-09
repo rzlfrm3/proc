85f1af03b0a9966c09b77c74d2b7c94413fce119
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-09 22:14:41 +0700

    Update model_nilai.php

--- application/models/model_nilai.php
+++ application/models/model_nilai.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_nilai extends CI_Model {
 	public function GetUser($where=""){
-		$data = $this->db->query('select * from tb_nilai '.$where);
+		$data = $this->db->query('select * from tb_nilai '.$where); //
 		return $data->result_array();
 	}
 	public function insertdata($tabelName,$data){
