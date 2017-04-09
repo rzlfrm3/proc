303200cc4009112fdb4b68522ed6e13bcb35b42a
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-09 22:13:38 +0700

    Update model_level.php

--- application/models/model_level.php
+++ application/models/model_level.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_level extends CI_Model {
 	public function GetUser($where=""){
-		$data = $this->db->query('select * from tb_level '.$where);
+		$data = $this->db->query('select * from tb_level '.$where); //
 		return $data->result_array();
 	}
 	public function insertdata($tabelName,$data){
