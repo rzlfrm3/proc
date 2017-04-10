d9ce58f661d0f8e75260050a5ce8bb10c7b2eea4
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-10 22:57:24 +0700

    Update subbidang.php

--- application/controllers/admin/subbidang.php
+++ application/controllers/admin/subbidang.php
@@ -5,7 +5,7 @@ class Subbidang extends CI_Controller {
 
 	public function index()
 	{
-		$data['title'] = 'Notifikasi seperti difacebook CodeIgniter';  gjgjhgjh
+		$data['title'] = 'Notifikasi seperti difacebook CodeIgniter';  
         $data['jlhnotif'] =$this->model_notifikasi->notif_count();  //menghitung jumlah post
         $data['notifikasi'] =$this->model_notifikasi->getnotifikasi(); //menampilkan isi postingan
 		$data['data'] = $this->model_subbidang->GetUser();
