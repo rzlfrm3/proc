404da7c0e64f09de7f8882ccfdfeea43d83e8a5b
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-13 22:28:00 +0700

    Update graph.php

--- application/controllers/admin/graph.php
+++ application/controllers/admin/graph.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Graph extends CI_Controller {
 	
 public function __construct()
-{
+{ //
 parent::__construct();
 $this->load->model('model_graph'); // 
 } fdjhsadgjdhasgd
