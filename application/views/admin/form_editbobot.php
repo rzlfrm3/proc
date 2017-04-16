4819602a63b6f8aeed0b565dab7b73d4f096a0b9
rzlfrm3 <rzlfrm3@gmail.com>
2017-04-16 22:20:29 +0700

    Update form_editbobot.php

--- application/views/admin/form_editbobot.php
+++ application/views/admin/form_editbobot.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> //
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
